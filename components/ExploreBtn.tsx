ef6ca458f6abed12e9b1c66e78e3844428eee01e
Senthil Kumar Rajendran <r.senthilkumar@outlook.com>
2025-12-30 17:57:04 -0600

    Added PostHog for Analytics

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const ExploreBtn = () => {
+  const handleClick = () => {
+    posthog.capture("explore_events_clicked", {
+      button_location: "hero_section",
+      target_section: "events",
+    });
+  };
+
   return (
     <button
       type="button"
       id="explore-btn"
       className="mt-7 mx-auto"
-      onClick={() => console.log("Clicked")}
+      onClick={handleClick}
     >
       <a href="#events">
         Explore More
